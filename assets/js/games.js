3d518d8c9880b30d39fa4385d8d4ec5b89e2c555
MHirano <michelle@hiranohomes.com>
2021-02-24 19:05:21 -0800

    start and end game functions

--- assets/js/games.js
+++ assets/js/games.js
@@ -69,6 +69,7 @@ var fight = function(enemyName) {
   }
 };
 
+var startGame = function() {
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
@@ -93,3 +94,30 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
   }
 }
+}
+
+var endGame = function() {
+  //if player is still alive, player wins
+
+  if (playerHealth > 0){
+    window.alert ("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+    else{
+      window.alert("You've lost your robot in battle.");
+    }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+  if (playAgainConfirm){
+    //restart game
+    startGame();
+  }
+  else{
+    window.alert("Thank you for playing! Come back soon!");
+  }
+}
+  
+
+
+
+//play again
+startGame();
